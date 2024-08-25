8063d0f66df8aaf38759de7ddad07f1e88d977fd
aquapi <aquaplmc@gmail.com>
2024-08-25 23:37:32 +0700

    Fix main.c

--- task1/main.c
+++ task1/main.c
@@ -1,9 +1,9 @@
-#include <stdint.h>
 #include <stdio.h>
 
 int main() {
   long long input;
-  scanf("Enter a number: %lld", &input);
+  printf("Enter a number: ");
+  scanf("%lld", &input);
   printf("%d", input % 12 == 0);
 
   return 0;
